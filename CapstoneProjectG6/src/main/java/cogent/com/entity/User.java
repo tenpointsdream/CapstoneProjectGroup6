0d5244a16f7a6610e1abefd3359ff2288847b53e
Khoa Ho <54515959+tenpointsdream@users.noreply.github.com>
2023-04-24 12:05:41 -0400

    User Entity commit (#2)
    Co-authored-by: tenpointsdream <tenpointsdream@hotmail>

---
+++ CapstoneProjectG6/src/main/java/cogent/com/entity/User.java
@@ -0,0 +1,28 @@
+package cogent.com.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String username;
+	private String password;
+	private String email;
+	private String userType;
+}
