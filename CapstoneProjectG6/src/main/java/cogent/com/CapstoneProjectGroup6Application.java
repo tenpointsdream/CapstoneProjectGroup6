f9cb80c92d5c95c9522a1075f262fcdc4b8cd278
tenpointsdream <tenpointsdream@hotmail>
2023-04-24 11:31:25 -0400

    First commit of the capstone project [backend]

---
+++ CapstoneProjectG6/src/main/java/cogent/com/CapstoneProjectGroup6Application.java
@@ -0,0 +1,13 @@
+package cogent.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectGroup6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectGroup6Application.class, args);
+	}
+
+}
---
+++ CapstoneProjectG6/src/test/java/cogent/com/CapstoneProjectGroup6ApplicationTests.java
@@ -0,0 +1,13 @@
+package cogent.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectGroup6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
